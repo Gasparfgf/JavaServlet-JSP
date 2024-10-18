2ffc40c64219023c80a391a2305d3b69d2d6bc47
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-10-18 19:15:08 +0200

    feat(MappingServlet): mapping servlet using urlPatterns annotation

--- src/main/java/com/example/jsptraining1/MappingServlet.java
+++ src/main/java/com/example/jsptraining1/MappingServlet.java
@@ -1,4 +1,33 @@
 package com.example.jsptraining1;
 
-public class MappingServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "mappingServlet", urlPatterns = "/servl-map")
+public class MappingServlet extends HttpServlet {
+
+    private String message;
+
+    public void init(){
+        message = "Learning how to map Servlet";
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("CALLED OUT!");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Mapping Servlet</h1>");
+        out.println("<p>This page's mapping servlet to 'URL Patterns' from a tag.</p>");
+        out.println("<footer>"+message+"</footer>");
+        out.println("</body></html>");
+    }
 }
