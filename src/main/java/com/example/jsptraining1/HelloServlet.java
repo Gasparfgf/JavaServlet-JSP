d2b61ee32d492d6d10b1d004ee9712f4634008dc
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-10-18 18:44:38 +0200

    fix: this is the true last commit

--- src/main/java/com/example/jsptraining1/HelloServlet.java
+++ src/main/java/com/example/jsptraining1/HelloServlet.java
@@ -1,9 +1,15 @@
 package com.example.jsptraining1;
 
-import java.io.*;
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
@@ -13,7 +19,7 @@ public class HelloServlet extends HttpServlet {
         message = "Hello GasparF!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
         // Hello
@@ -21,6 +27,22 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        System.out.println("Post servlet request invoked !");
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        JsonObject json = new JsonObject();
+        json.addProperty("request-type", "post");
+        json.addProperty("status", 200);
+        json.addProperty("content", "your post request has succeeded !");
+
+        PrintWriter out = response.getWriter();
+        out.println("Servlet Response");
+        out.println(json.toString());
     }
 
     public void destroy() {
